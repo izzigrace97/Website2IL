e8d516c4c57f758bae9b2dc99e33f74ffc25354e
izzigrace97 <63287742+izzigrace97@users.noreply.github.com>
2020-06-06 09:19:41 +0100

    Adding in baking function

--- script.js
+++ script.js
@@ -10,6 +10,10 @@ function myFunction2() {
   document.getElementById("myDropdown2").classList.toggle("show");
 }
 
+function myFunction3() {
+  document.getElementById("myDropdown3").classList.toggle("show");
+}
+
 // Close the dropdown if the user clicks outside of it
 window.onclick = function(e) {
   if (!e.target.matches('.dropbtn')) {
@@ -23,6 +27,15 @@ window.onclick = function(e) {
 window.onclick = function(e) {
   if (!e.target.matches('.dropbtn')) {
   var myDropdown = document.getElementById("myDropdown2");
+    if (myDropdown.classList.contains('show')) {
+      myDropdown.classList.remove('show');
+    }
+  }
+}
+
+window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn')) {
+  var myDropdown = document.getElementById("myDropdown3");
     if (myDropdown.classList.contains('show')) {
       myDropdown.classList.remove('show');
     }
@@ -73,3 +86,4 @@ const init = () => {
 }
 
 init();
+
